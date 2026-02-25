75e0387de758201026cf2999dd9b120073032883
thuantrink <trinhtanthuan22@gmail.com>
2026-02-25 19:41:44 +0700

    Danh sách yêu cầu Rescue requests
    Create /rescue-coordinator/requests

---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      localStorage.removeItem('user')
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default api
